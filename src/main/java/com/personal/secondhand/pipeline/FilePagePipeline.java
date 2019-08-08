7351c50ede26f5b1a999094b3cd927023bba242c
sd4000784 <sd4000784@qq.com>
2019-08-09 00:36:58 +0800

    文件格式变更

--- src/main/java/com/personal/secondhand/pipeline/FilePagePipeline.java
+++ src/main/java/com/personal/secondhand/pipeline/FilePagePipeline.java
@@ -1,7 +1,6 @@
 package com.personal.secondhand.pipeline;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.lang3.StringUtils;
 import us.codecraft.webmagic.ResultItems;
 import us.codecraft.webmagic.Task;
@@ -12,42 +11,36 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 
-/**
- * 将五八详情页html内容下载到本地
- * Created by yangrui on 2019-8-7.
- *
- * @author yangrui
- */
 @Slf4j
-public class WBFilePipeline extends FilePipeline {
-
-    public WBFilePipeline() {
+public class FilePagePipeline extends FilePipeline {
+    public FilePagePipeline() {
         super();
     }
 
-    public WBFilePipeline(String path) {
+    public FilePagePipeline(String path) {
         super(path);
     }
 
     @Override
     public void process(ResultItems resultItems, Task task) {
         // 从PageProcess获取设置的html内容
-        String html = resultItems.get("html");
+        String html = resultItems.get("pageHtml");
         if (StringUtils.isBlank(html)) {
             // 没有就跳出
             return;
         }
         String url = resultItems.getRequest().getUrl();
-        url = url.replaceAll("https://", "").replaceAll("\\?", "#");
+        url = url.replaceAll("https://", "").replaceAll("\\?", "#").replaceAll("/", "_");
         System.out.println(url);
         // 以下内容参考至FilePipeline#process 替换了写入内容
-        String path = super.path + PATH_SEPERATOR + url + PATH_SEPERATOR;
+        String path = super.path + PATH_SEPERATOR + "pageHtml" + PATH_SEPERATOR + url ;
         try {
-            PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(new FileOutputStream(this.getFile(path + DigestUtils.md5Hex(resultItems.getRequest().getUrl()) + ".html")), "UTF-8"));
+//            PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(new FileOutputStream(this.getFile(path + DigestUtils.md5Hex(resultItems.getRequest().getUrl()) + ".html")), "UTF-8"));
+            PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(new FileOutputStream(this.getFile(path + ".html")), "UTF-8"));
             printWriter.println(html);
             printWriter.close();
         } catch (IOException e) {
-            log.error("html文件写入异常", e);
+            log.error("page html文件写入异常", e);
         }
     }
 }
--- src/main/java/com/personal/secondhand/processor/MGPageProcessor.java
+++ src/main/java/com/personal/secondhand/processor/MGPageProcessor.java
@@ -1,7 +1,8 @@
 package com.personal.secondhand.processor;
 
 import com.personal.secondhand.constants.CommonConstants;
-import com.personal.secondhand.pipeline.WBFilePipeline;
+import com.personal.secondhand.pipeline.FileInfoPipeline;
+import com.personal.secondhand.pipeline.FilePagePipeline;
 import com.personal.secondhand.util.JsoupUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
@@ -32,7 +33,7 @@ public class MGPageProcessor implements PageProcessor {
     private static WebDriver driver;
 
     static {
-        System.setProperty("phantomjs.binary.path", "D:\\yangrui\\git\\secondhand\\party\\phantomjs.exe");
+        System.setProperty("phantomjs.binary.path", "F:\\workspace\\secondhand\\party\\phantomjs.exe");
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("takesScreenshot", false);
         capabilities.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, new String[]{
@@ -59,7 +60,7 @@ public class MGPageProcessor implements PageProcessor {
             .addHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
             .addHeader("Cookie", "UM_distinctid=16c6bfe67207d1-009a7b1084046a8-4c312272-1fa400-16c6bfe6721ee; CNZZDATA1272232378=1280959118-1565178473-https%253A%252F%252Fwww.baidu.com%252F%7C1565188315; Hm_lvt_1f8e83330039496a701d7ae44836ed10=1565179931; Hm_lpvt_1f8e83330039496a701d7ae44836ed10=1565190075; Hm_cv_1f8e83330039496a701d7ae44836ed10=1*userType*visitor; BPC=c56cc9984d87ac78d87bfcaa545f8b1d; website_qiehuan_cityid=1; ASP.NET_SessionId=nzal3n1extpi4yq3srsnn41h; historySellHouse=3213612|https://img76.517cdn.com/up2019/mg/2019/8/5/17/house/viewimg/fid3213612_uid32839_20190805175338343ade.jpg.2911.524x393.0.jpg|è¶\u008Aç§\u0080æ\u0098\u009Fæ±\u0087äº\u0091é\u0094¦ä¸\u0080æ\u009C\u009F ä¸¤å®¤ä¸¤å\u008E\u0085ä¸¤å\u008D« æ²³æ\u0099¯æ\u0088¿ |245.00|2|2|2|123.19|å\u008D\u0097||19888.00ã\u0080\u00852657111|https://img76.517cdn.com/up2019/mg/2019/7/2/12/house/viewimg/fid2657111_uid35581_20190702120104325865.jpg.2911.524x393.0.jpg|å\u008A³å\u008A¨å\u0085¬å\u009B\u00ADæ\u0097\u0081 ç²¾è£\u0085ä¸¤å®¤ å\u009C°ç\u0083\u00ADæ\u0088¿ èµ é\u0080\u0081å\u0085¨å±\u008Bå®¶å\u0085·å®¶ç\u0094µ é\u009A\u008Fæ\u0097¶ç\u009C\u008B|53.00|2|1|1|64.24|ä¸\u009Cè¥¿||8250.00ã\u0080\u00852889097|https://g.517cdn.com/www517cn/2016v1/images/noimg_big.png|é\u0095¿æ±\u009Få\u008D\u0097å°\u008Få\u008Cº å\u008D\u0097å\u0090\u0091 ä¸\u008Dæ\u008A\u008Aå±±ä¸\u008Dä¸´è¡\u0097|30.00|1|1|1|38.70|å\u008D\u0097||7752.00ã\u0080\u00853003919%7chttps%3a%2f%2fimg76.517cdn.com%2fup2019%2fmg%2f2019%2f7%2f31%2f14%2fhouse%2fviewimg%2ffid3003919_uid51209_201907311440179b5810.jpg.2911.524x393.0.jpg%7c%e5%85%b4%e9%be%99%e8%8b%912%e6%a5%bc%e4%b8%8d%e6%8a%8a%e5%b1%b1%e4%b8%8d%e4%b8%b4%e8%a1%97%e5%8d%97%e5%8c%97%e6%a0%87%e6%88%b7%ef%bc%81%7c126.00%7c2%7c1%7c1%7c112.53%7c%e5%8d%97%e5%8c%97%7c%7c11197.00%e3%80%85; web_views_count_xq=[{\"Id\":541,\"Sc\":1,\"Rc\":0,\"Xc\":0,\"Count\":2,\"Lt\":\"\\/Date(1565190070331)\\/\"},{\"Id\":540,\"Sc\":1,\"Rc\":0,\"Xc\":0,\"Count\":2,\"Lt\":\"\\/Date(1565189857052)\\/\"},{\"Id\":10034,\"Sc\":2,\"Rc\":0,\"Xc\":0,\"Count\":4,\"Lt\":\"\\/Date(1565181230324)\\/\"},{\"Id\":10683,\"Sc\":1,\"Rc\":0,\"Xc\":0,\"Count\":2,\"Lt\":\"\\/Date(1565180502739)\\/\"}]; ExtensionUserID=51209; VerifyCode=av4b; website_cityid=1; website_cityname=%e6%b2%88%e9%98%b3")
             .setRetryTimes(3)
-            .setSleepTime(CommonConstants.TIME_OUT)
+            .setTimeOut(CommonConstants.TIME_OUT)
             .setCycleRetryTimes(3);
 
     @Override
@@ -88,13 +89,16 @@ public class MGPageProcessor implements PageProcessor {
 //                String pid = linkman.replaceAll("/s", "").replaceAll("/", "");
                 page.addTargetRequest(nextUrl);
             }
+
+            page.putField("pageHtml", html);
+
         } else if (page.getUrl().regex("https://www\\.517\\.cn/ershoufang/[0-9]{7}/*").match()) {
             log.info("详情页{}", url);
             Document document = JsoupUtils.parse(html);
             String cssPath = "html.ng-scope body.ng-scope form#form1.ng-pristine.ng-valid div.mainwrapper div.houseBOX.width960 div.h-public.clearfix div.h-info-box div.m-jjr.clearfix div.m-jjr-nt div.m-jjr-o.clearfix ul li.m-jjr-name a";
             String pid = document.select(cssPath).attr("href");
             System.out.println(pid);
-            page.putField("html", html);
+            page.putField("infoHtml", html);
             page.putField("pid", pid);
         } else {
             log.error("未知页面{}", url);
@@ -160,10 +164,11 @@ public class MGPageProcessor implements PageProcessor {
                 .setUUID(today);
 
 
-        for (int i = 1; i <= 1; i++) {
+        for (int i = 1; i <= 100; i++) {
             spider.addUrl("https://www.517.cn/ershoufang/osj1/area/pg" + i + "?ckattempt=1");
         }
-        spider.addPipeline(new WBFilePipeline("d:/517html/"));
+        spider.addPipeline(new FilePagePipeline("d:/517html/" + today));
+        spider.addPipeline(new FileInfoPipeline("d:/517html/" + today));
         spider.thread(4);
         spider.run();
 
